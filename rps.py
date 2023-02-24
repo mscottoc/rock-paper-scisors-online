3a953a9a7da8a1da3c162ac00708841117607dd2
mscottoc <michaeloconnor20@outlook.com>
2023-02-23 23:07:04 -0700

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,3 @@
+print("RULES: rock beats scissors, scissors beats paper, paper beats rock.")
+print("The rules are best out of three.")
+print("If you tie after three rounds, the game will keep going until one of you win.")
